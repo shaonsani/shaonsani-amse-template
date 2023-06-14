965f48a9015086aa269182ea57b23d8fa4573963
Md Shaon Hossain Sani <mdshaonhossain.sani@groupm.com>
2023-06-14 17:43:08 +0200

    Solve Exercise 3

---
+++ exercises/exercise3.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import sqlalchemy
+
+class Exercise3:
+    def __init__(self):
+        self.data_url = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
+        self.engine = sqlalchemy.create_engine("sqlite:///cars.sqlite")
+
+    def load_data(self):
+        self.data = pd.read_csv(self.data_url, 
+                                sep=';',
+                                encoding='iso-8859-1',
+                                skiprows=6,
+                                skipfooter=4,
+                                engine='python')
+
+    def data_preprocessing(self):
+        self.data = self.data.iloc[:, [0, 1, 2, 12, 22, 33, 42, 52, 61, 69]]
+        self.data = self.data.rename(columns={
+            self.data.columns[0]: 'date',
+            self.data.columns[1]: 'CIN',
+            self.data.columns[2]: 'name',
+            self.data.columns[3]: 'petrol',
+            self.data.columns[4]: 'diesel',
+            self.data.columns[5]: 'gas',
+            self.data.columns[6]: 'electro',
+            self.data.columns[7]: 'hybrid',
+            self.data.columns[8]: 'plugInHybrid',
+            self.data.columns[9]: 'others'
+        })
+
+        self.data['CIN'] = self.data['CIN'].astype(str).str.zfill(5)
+        # Validation for positive integers greater than 0
+        valid_columns = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
+        for col in valid_columns:
+            self.data[col] = pd.to_numeric(self.data[col], errors='coerce')
+            self.data = self.data[self.data[col] > 0]
+
+    def save_to_database(self):
+        column_types = {
+                    'date': sqlalchemy.Date,
+                    'CIN': sqlalchemy.String(255),
+                    'name': sqlalchemy.String(255),
+                    'petrol': sqlalchemy.Integer,
+                    'diesel': sqlalchemy.Integer,
+                    'gas': sqlalchemy.Integer,
+                    'electro': sqlalchemy.Integer,
+                    'hybrid': sqlalchemy.Integer,
+                    'plugInHybrid': sqlalchemy.Integer,
+                    'others': sqlalchemy.Integer
+                }
+        self.data.to_sql(name="cars", con=self.engine, if_exists="replace", index=False, dtype=column_types)
+
+    def solve(self):
+        self.load_data()
+        self.data_preprocessing()
+        self.save_to_database()
+
+obj = Exercise3()
+obj.solve()
