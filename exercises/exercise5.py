89673ce204e639defbe2ac62e8ddb2652a2af7ce
Md Shaon Hossain Sani <mdshaonhossain.sani@groupm.com>
2023-07-06 00:39:21 +0200

    Refactor exercise 5

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -1,4 +1,5 @@
-import requests, zipfile, io
+import zipfile
+import urllib.request
 import pandas as pd
 import sqlalchemy
 
@@ -6,20 +7,21 @@ class Exercise5:
     def __init__(self):
         self.data_url = "https://gtfs.rhoenenergie-bus.de/GTFS.zip"
         self.engine = sqlalchemy.create_engine("sqlite:///gtfs.sqlite")
+        self.zip_filename = "GTFS.zip"
         self.data = None
 
     def load_data(self):
-        r = requests.get(self.data_url)
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        z.extractall("./exercises/data/")
+        urllib.request.urlretrieve(self.data_url, self.zip_filename)
+        with zipfile.ZipFile(self.zip_filename, 'r') as zip:
+            zip.extractall("./exercises/data/")
     
-    def steps(self):
+    def validation(self):
         self.data = pd.read_csv("./exercises/data/stops.txt", sep=",")
         self.data = self.data[["stop_id", "stop_name", "stop_lat", "stop_lon", "zone_id"]]
         self.data = self.data[self.data["zone_id"] == 2001]
         self.data = self.data[(self.data["stop_lat"] >= -90) & (self.data["stop_lat"] <= 90)]
         self.data = self.data[(self.data["stop_lon"] >= -90) & (self.data["stop_lon"] <= 90)]
-        self.data = self.data[self.data['stop_name'].str.contains('[^a-zA-Z0-9äöüÄÖÜß]+')]
+        #self.data = self.data[self.data['stop_name'].str.contains('[^a-zA-Z0-9äöüÄÖÜß]+')]
     
     def save_to_database(self):
         column_types = {
@@ -32,8 +34,11 @@ class Exercise5:
         self.data.to_sql(name="stops", con=self.engine, if_exists="replace", index=False, dtype=column_types)
 
     def solve(self):
+        print("Solving Exercise 5.........!")
+        print("Loading Data.........!")
         self.load_data()
-        self.steps()
+        print("Validating Data.........!")
+        self.validation()
         self.save_to_database()
 
 
